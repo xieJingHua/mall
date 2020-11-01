4eae24985a63e0deba910efd783777d7784e1c1e
xieJingHua <xiejh888@gmail.com>
2020-11-01 14:27:50 +0800

    update  gateway网关

---
+++ mall-gateway/src/main/java/com/xiejh/gateway/MallGatewayApplication.java
@@ -0,0 +1,15 @@
+package com.xiejh.gateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+
+
+@SpringBootApplication(exclude = DataSourceAutoConfiguration.class)
+public class MallGatewayApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MallGatewayApplication.class, args);
+    }
+
+}
