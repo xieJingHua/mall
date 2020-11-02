40dc0a42f872ae8ff221c32e10a935043217dd2d
xieJingHua <xiejh888@gmail.com>
2020-11-02 17:25:07 +0800

    add 跨域配置

---
+++ mall-gateway/src/main/java/com/xiejh/gateway/config/CrossOriginConfig.java
@@ -0,0 +1,31 @@
+package com.xiejh.gateway.config;
+
+import org.checkerframework.checker.units.qual.C;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.reactive.CorsWebFilter;
+import org.springframework.web.cors.reactive.UrlBasedCorsConfigurationSource;
+
+/**
+ * @author xiejh
+ * @Date 2020/11/2 16:45
+ **/
+@Configuration
+public class CrossOriginConfig {
+
+    @Bean
+    public CorsWebFilter corsWebFilter(){
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        CorsConfiguration configuration = new CorsConfiguration();
+        configuration.addAllowedHeader("*");
+        configuration.addAllowedMethod("*");
+        configuration.addAllowedOrigin("*");
+        configuration.setAllowCredentials(true);
+
+        source.registerCorsConfiguration("/**",configuration);
+
+        return new CorsWebFilter(source);
+    }
+}
--- renren-generator/src/main/java/io/renren/RenrenApplication.java
+++ renren-generator/src/main/java/io/renren/RenrenApplication.java
@@ -5,7 +5,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.data.mongo.MongoDataAutoConfiguration;
 import org.springframework.boot.autoconfigure.mongo.MongoAutoConfiguration;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
+
+@EnableDiscoveryClient
 @SpringBootApplication(exclude = {MongoAutoConfiguration.class, MongoDataAutoConfiguration.class})
 @MapperScan("io.renren.dao")
 public class RenrenApplication {
