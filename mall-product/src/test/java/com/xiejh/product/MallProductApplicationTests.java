561f3305d5cfc6d153341685549259c8719bb79a
xieJingHua <xiejh888@gmail.com>
2020-11-08 15:32:55 +0800

    udpate 整合redis，并测试

--- mall-product/src/test/java/com/xiejh/product/MallProductApplicationTests.java
+++ mall-product/src/test/java/com/xiejh/product/MallProductApplicationTests.java
@@ -5,13 +5,16 @@ import com.xiejh.product.service.BrandService;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.data.redis.core.StringRedisTemplate;
+import org.springframework.data.redis.core.ValueOperations;
 
 @SpringBootTest
 class MallProductApplicationTests {
 
     @Autowired
     BrandService brandService;
-
+    @Autowired
+    StringRedisTemplate  stringRedisTemplate;
 
     @Test
     void contextLoads() {
@@ -20,6 +23,14 @@ class MallProductApplicationTests {
         brandEntity.setName("小米");
         brandService.save(brandEntity);
         System.out.println("保存成功");
+    }
+
+    @Test
+    void testRedis(){
+        ValueOperations<String, String> ops = stringRedisTemplate.opsForValue();
+        ops.set("hello","world");
+        String hello = ops.get("hello");
+        System.out.println(hello);
     }
 
 }
