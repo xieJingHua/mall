03ca3e3c515a630bb61fa1929501e67ec29d0b84
xieJingHua <xiejh888@gmail.com>
2020-11-11 17:16:30 +0800

    add 认证服务

---
+++ mall-auth/src/main/java/com/xiejh/mallauth/config/MallWebConfig.java
@@ -0,0 +1,22 @@
+package com.xiejh.mallauth.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+/**
+ * @author xiejh
+ * @Date 2020/11/11 16:15
+ **/
+@Configuration
+public class MallWebConfig implements WebMvcConfigurer {
+
+    /**
+     * 视图映射
+     * @param registry
+     */
+    @Override
+    public void addViewControllers(ViewControllerRegistry registry) {
+        registry.addViewController("/login.html").setViewName("login");
+    }
+}
---
+++ mall-auth/src/test/java/com/xiejh/mallauth/MallAuthApplicationTests.java
@@ -0,0 +1,13 @@
+package com.xiejh.mallauth;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MallAuthApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
