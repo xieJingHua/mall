6ca8869665a096a08101b94a7d20341088800430
xieJingHua <xiejh888@gmail.com>
2020-11-12 23:40:08 +0800

    update spring session

---
+++ mall-auth/src/main/java/com/xiejh/mallauth/config/MallSessionConfig.java
@@ -0,0 +1,30 @@
+package com.xiejh.mallauth.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.redis.serializer.GenericJackson2JsonRedisSerializer;
+import org.springframework.data.redis.serializer.RedisSerializer;
+import org.springframework.session.web.http.CookieSerializer;
+import org.springframework.session.web.http.DefaultCookieSerializer;
+
+/**
+ * @author xiejh
+ * @Date 2020/11/12 16:20
+ **/
+@Configuration
+public class MallSessionConfig {
+
+
+    @Bean
+    public CookieSerializer cookieSerializer() {
+        DefaultCookieSerializer serializer = new DefaultCookieSerializer();
+        serializer.setCookieName("JSESSIONID");
+        serializer.setDomainName("mall.com");
+        return serializer;
+    }
+
+    @Bean
+    public RedisSerializer<Object> springSessionDefaultRedisSerializer() {
+        return new GenericJackson2JsonRedisSerializer();
+    }
+}
