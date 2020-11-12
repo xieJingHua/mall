5149fec9f755841706c3bde6136478af8a7282fe
xieJingHua <xiejh888@gmail.com>
2020-11-12 08:44:54 +0800

    update 登录加盐

--- mall-user/src/test/java/com/xiejh/user/MallUserApplicationTests.java
+++ mall-user/src/test/java/com/xiejh/user/MallUserApplicationTests.java
@@ -1,13 +1,30 @@
 package com.xiejh.user;
 
+import org.apache.commons.codec.digest.DigestUtils;
+import org.apache.commons.codec.digest.Md5Crypt;
+import org.bouncycastle.crypto.generators.BCrypt;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
 @SpringBootTest
 class MallUserApplicationTests {
 
     @Test
     void contextLoads() {
+
     }
 
+    public static void main(String[] args) {
+        String s = DigestUtils.md5Hex("123456");
+        System.out.println(s);
+        //md5加密，摘要加密，加盐
+        Md5Crypt.md5Crypt("123456".getBytes());
+
+        //spring加密
+        BCryptPasswordEncoder encoder = new BCryptPasswordEncoder();
+        String encode = encoder.encode("123456");
+        System.out.println(encode);
+        System.out.println(encoder.matches("123456",encode));
+    }
 }
